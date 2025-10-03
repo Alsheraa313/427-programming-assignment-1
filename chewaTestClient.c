659551525382764a3e80367ad8e5552dcabc0e51
Elfykins123 <efoucher789@gmail.com>
2025-10-02 23:44:04 -0400

    Cross-Platform code
    Added in some extra code that should make this program cross-platform between both Windows and Linux.

--- chewaTestClient.c
+++ chewaTestClient.c
@@ -2,47 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
 #include <unistd.h> 
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#endif
 
-int main(){
+int main() {
 
     int netSocket;
+    char serverResponse[256] = { 0 };
+    char clientMessage[256] = { 0 };
 
-    char serverResponse[256] ={0};
-    char clientMessage[256] = {0};
+#ifdef _WIN32
+    // Initialize Winsock
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed to initialize Winsock\n");
+        return 1;
+    }
+#endif
 
     netSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(9001);
-    server_address.sin_addr.s_addr = INADDR_ANY;
 
-    int connectStatus = connect(netSocket, (struct sockaddr *) &server_address, sizeof(server_address));
+    // Cross-platform way to safely set loopback address
+#ifdef _WIN32
+    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+#else
+    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+#endif
 
-    if(connectStatus < 0){
-        printf("something went wrong while connecting");
+    int connectStatus = connect(netSocket, (struct sockaddr*)&server_address, sizeof(server_address));
+
+    if (connectStatus < 0) {
+        perror("Connection failed");
+#ifdef _WIN32
+        closesocket(netSocket);
+        WSACleanup();
+#else
+        close(netSocket);
+#endif
         return 1;
     }
 
     printf("enter message to send to server: ");
 
     fgets(clientMessage, sizeof(clientMessage), stdin);
-
     clientMessage[strcspn(clientMessage, "\n")] = 0;
 
-    send(netSocket, clientMessage, strlen(clientMessage)+1, 0);
+    send(netSocket, clientMessage, strlen(clientMessage) + 1, 0);
+    recv(netSocket, serverResponse, sizeof(serverResponse), 0);
 
-    recv(netSocket, &serverResponse, sizeof(serverResponse), 0);
+    printf("%s\n", serverResponse);
 
-    printf(serverResponse);
-
+#ifdef _WIN32
+    closesocket(netSocket);
+    WSACleanup();
+#else
     close(netSocket);
+#endif
+
     return 0;
 }
-
--- chewaTestServer.c
+++ chewaTestServer.c
@@ -2,16 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <unistd.h> 
-
+// Platform-specific includes
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#endif
 
 int main() {
 
-    char clientMessage[256] = {0};
-    const char *serverMessage = "hello i am server";
+    char clientMessage[256] = { 0 };
+    const char* serverMessage = "hello i am server";
+
+#ifdef _WIN32
+    // Initialize Winsock
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed to initialize Winsock\n");
+        return 1;
+    }
+#endif
+
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in serverAddress;
@@ -19,8 +35,8 @@ int main() {
     serverAddress.sin_port = htons(9001);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)); 
-     
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
     listen(serverSocket, 5);
 
     printf("server is listening\n");
@@ -28,39 +44,48 @@ int main() {
     int clientSocket = accept(serverSocket, NULL, NULL);
     if (clientSocket < 0) {
         perror("connection failed");
+#ifdef _WIN32
+        closesocket(serverSocket);
+        WSACleanup();
+#else
         close(serverSocket);
+#endif
         exit(1);
     }
 
+    recv(clientSocket, clientMessage, sizeof(clientMessage), 0);
 
-recv(clientSocket, clientMessage, sizeof(clientMessage), 0);
+    if (strcmp(clientMessage, "buy") == 0) {
 
-if (strcmp(clientMessage, "buy") == 0) {
+        send(clientSocket, "input recived was 'buy'", strlen("input recived was 'buy'"), 0);
 
-send(clientSocket, "input recived was 'buy'", strlen("input recived was buy"), 0);
+        /*
+        here ask for which card they want to buy, check if its in the array, if its valid, pop it outta the array
+        */
 
-    /*
-    here ask for which card they want to buy, check if its in the array, if its valid, pop it outta the array
-    */
+    }
 
-}
+    else if (strcmp(clientMessage, "sell") == 0) {
 
-else if (strcmp(clientMessage, "sell") == 0) {
+        send(clientSocket, "input received was 'sell'", strlen("input received was 'sell'"), 0);
+        /*
+        here just add whatever they type into a constructor that makes the card like
+        "what would you like to sell(name, price, amount)?:
+        and the input would be like
+        pikachu, 10, 1
+        doesnt have to be too sophisticated
+        */
 
-send(clientSocket, "input received was 'sell'", strlen("input received was 'sell'"), 0);
-    /*
-    here just add whatever they type into a constructor that makes the card like 
-    "what would you like to sell(name, price, amount)?: 
-    and the input would be like 
-    pikachu, 10, 1
-    doesnt have to be too sophisticated 
-    
-    */
+    }
 
-}
-
+#ifdef _WIN32
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+#else
     close(clientSocket);
     close(serverSocket);
+#endif
 
     return 0;
 }
